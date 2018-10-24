6890feec2a754dc505e2d8e83ff79354243e27a2
Vyacheslav Solomin <slel64222@gmail.com>
2018-10-24 23:37:52 +0300

    а если так

--- webpack.config.js
+++ webpack.config.js
@@ -15,130 +15,135 @@ const BrotliPlugin = require('brotli-webpack-plugin');
 
 // module settings
 module.exports = {
-        context: path.resolve(__dirname, 'src'),
+    context: path.resolve(__dirname, 'src'),
 
-        entry: {
-            bundle: [
-                './js/libs.js',
-                './js/scripts.js',
+    entry: {
+        bundle: [
+            './js/libs.js',
+            './js/scripts.js',
 
-                './css/common.css',
-                './css/pt_sans.css',
-                './css/bootstrap.css',
-            ],
-            // libs: [
-            //     './js/libs.js',
-            // ]
-        },
-        // TODO: наверное не правильно, но в данном случае думаю лучше вместо dist использовать просто корень (типо как и было в исходном варианте)
-        output: {
-            filename: 'scripts.[name].js',
-            path: path.resolve(__dirname),
-            publicPath: '../'
-        },
-
-        plugins: [
-            // new CleanWebpackPlugin(['css','js']),
-
-            // new BundleAnalyzerPlugin(),
-
-            new CopyWebpackPlugin([{
-                    from: './assets/img',
-                    to: 'assets'
-                },
-                {
-                    from: './assets/svg',
-                    to: 'assets'
-                },
-                {
-                    from: './assets/fonts',
-                    to: 'fonts'
-                }
-
-            ]),
-
-            new ImageminPlugin({
-                test: /\.(jpe?g|png|webp|svg)$/i,
-            }),
-
-            new MiniCssExtractPlugin({
-                filename: 'styles.css',
-            }),
-
-            new BrotliPlugin({
-                asset: '[path].br[query]',
-                test: /\.(js|css|html|svg|png|jpe?g)$/,
-                threshold: 10240,
-                minRatio: 0.8
-            })
+            './css/common.css',
+            // './css/pt_sans.css',
+            // './css/bootstrap.css',
         ],
+        // libs: [
+        //     './js/libs.js',
+        // ]
+    },
+    // TODO: наверное не правильно, но в данном случае думаю лучше вместо dist использовать просто корень (типо как и было в исходном варианте)
+    output: {
+        filename: 'scripts.[name].js',
+        path: path.resolve(__dirname),
+        publicPath: '../'
+    },
 
-        optimization: {
-            minimizer: [
-                new UglifyJsPlugin({
-                    cache: true,
-                    parallel: true,
-                    extractComments: true
-                }),
-                new OptimizeCSSAssetsPlugin({})
-            ]
-        },
+    plugins: [
+        // new CleanWebpackPlugin(['css','js']),
 
-        module: {
-            rules: [
-                // babel
-                {
-                    test: /\.js$/,
-                    exclude: /(node_modules|bower_components)/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            cacheDirectory: true,
-                            presets: ['@babel/preset-env']
-                        }
+        // new BundleAnalyzerPlugin(),
+
+        new CopyWebpackPlugin([{
+                from: './assets/img',
+                to: 'assets'
+            },
+            {
+                from: './assets/svg',
+                to: 'assets'
+            },
+            {
+                from: './assets/fonts',
+                to: 'fonts'
+            }
+
+        ]),
+
+        new ImageminPlugin({
+            test: /\.(jpe?g|png|webp|svg)$/i,
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+        }),
+
+        new BrotliPlugin({
+            asset: '[path].br[query]',
+            test: /\.(js|css|html|svg|png|jpe?g)$/,
+            threshold: 10240,
+            minRatio: 0.8
+        })
+    ],
+
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                extractComments: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+
+    module: {
+        rules: [
+            // babel
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                        presets: ['@babel/preset-env']
                     }
-                },
+                }
+            },
 
-                // css
-                {
-                    test: /\.css$/,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        { loader: 'css-loader', options: { url: false } },
-                    ]
-                },
-
-                    // Img TODO: таксс.... не работает и свг тоже =(
+            // css
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
                     {
-                        test: /\.(png|jpeg)$/,
-                        loaders: [{
-                                loader: 'file-loader',
-                                options: {
-                                    name: '[path][name].[ext]'
-                                },
-                            },
-                            'img-loader'
-                        ],
+                        loader: 'css-loader',
+                        options: {
+                            url: false
+                        }
                     },
+                ]
+            },
 
-                    // svg
-                    {
-                        test: /\.svg/,
-                        loader: 'svg-url-loader',
-                        options: {}
+            // Img TODO: таксс.... не работает и свг тоже =(
+            {
+                test: /\.(png|jpeg)$/,
+                loaders: [{
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]'
+                        },
                     },
-
-                    // fonts
-                    {
-                        test: /\.(woff | woff2 | eot | ttf | of )/,
-                        use: [{
-                            loader: 'file-loader',
-                            options: {
-                                name: '[path][name].[ext]'
-                            },
-                        }]
-                    },
+                    'img-loader'
                 ],
             },
 
-        }
+            // svg
+            {
+                test: /\.svg/,
+                loader: 'svg-url-loader',
+                options: {}
+            },
+
+            // fonts
+            {
+                test: /\.(woff | woff2 | eot | ttf | of )/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[path][name].[ext]'
+                    },
+                }]
+            },
+        ],
+    },
+
+}
