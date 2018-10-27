a6dc99163838811e70565bc4a4bd8449979241dc
Vyacheslav Solomin <slel64222@gmail.com>
2018-10-27 14:00:20 +0300

    хм хм хм

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -237,26 +237,28 @@
             });
         }
     });
-    const filterSelect = document.querySelector('.filter__select');
-    const selectButton = filterSelect.querySelector('.filter__select-button');
-    const selectButtonText = filterSelect.querySelector('.filter__select-button .button__text');
-    const selectOptions = filterSelect.querySelectorAll('.filter__select-item');
-    const popup = filterSelect.querySelector('.filter__select-popup');
+    
+    // Тоже не надо
+    // const filterSelect = document.querySelector('.filter__select');
+    // const selectButton = filterSelect.querySelector('.filter__select-button');
+    // const selectButtonText = filterSelect.querySelector('.filter__select-button .button__text');
+    // const selectOptions = filterSelect.querySelectorAll('.filter__select-item');
+    // const popup = filterSelect.querySelector('.filter__select-popup');
 
-    selectButton.addEventListener('click', function () {
-        popup.classList.toggle('filter__select-popup_open');
-    });
+    // selectButton.addEventListener('click', function () {
+    //     popup.classList.toggle('filter__select-popup_open');
+    // });
 
-    selectOptions.forEach(o => {
-        o.addEventListener('click', function (e) {
-            document.querySelector('#' + e.target.dataset.group).checked = true;
+    // selectOptions.forEach(o => {
+    //     o.addEventListener('click', function (e) {
+    //         document.querySelector('#' + e.target.dataset.group).checked = true;
 
-            selectOptions.forEach(opt => opt.classList.toggle('filter__select-item_checked', false));
-            e.target.classList.toggle('filter__select-item_checked', true);
-            popup.classList.toggle('filter__select-popup_open', false);
-            selectButtonText.innerText = e.target.innerText;
-        })
-    });
+    //         selectOptions.forEach(opt => opt.classList.toggle('filter__select-item_checked', false));
+    //         e.target.classList.toggle('filter__select-item_checked', true);
+    //         popup.classList.toggle('filter__select-popup_open', false);
+    //         selectButtonText.innerText = e.target.innerText;
+    //     })
+    // });
 
     // Вот эта штука не используется
     // document.querySelector('.menu__icon').addEventListener('click', function () {
